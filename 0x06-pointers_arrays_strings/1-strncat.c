217ec3431a7ca952c6a6579b04f9d5b9a77c7d12
Sikansuo <ritawusulu5@gmail.com>
2023-06-29 19:17:45 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,31 @@
 #include "main.h"
-
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: 0 or 1
+ * Return: dest
  */
-
-int _isdigit(int c)
+char *_strncat(char *dest, char *src, int n)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
