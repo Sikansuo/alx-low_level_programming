a6d99414e4e504fd731288d25c0fe97823ab19e0
Rita Wusulu Issaka <ritawusulu5@gmail.com>
2023-08-11 20:06:48 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,14 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
@@ -20,15 +31,4 @@
 
 	return (1);
 }
-#include "main.h"
-
-/**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
- */
-int create_file(const char *filename, char *text_content)
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,5 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	close(o);
 
-	
+	return (1);
+}
+
 
